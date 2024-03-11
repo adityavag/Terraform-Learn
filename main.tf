9a4882dce8ba006e41d0264fb1c3fdc0c10098c0
adi247u <adi247u@gmail.com>
2024-03-11 22:33:12 +0530

    Creation of S3 Bucket Using Terraform

--- main.tf
+++ main.tf
@@ -2,10 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my_instance" {
-  ami = "ami-0f403e3180720dd7e"
-  instance_type = "t2.micro"
-  tags = {
-    "Name" = "HelloTerraform"
-  }
+# resource "aws_instance" "my_instance" {
+#   ami = "ami-0f403e3180720dd7e"
+#   instance_type = "t2.micro"
+#   tags = {
+#     "Name" = "HelloTerraform"
+#   }
+# }
+
+resource "aws_s3_bucket" "mybucket" {
+    tags = {
+        Name = "buck"
+        Environment = "Dev"
+    }
+  
 }
